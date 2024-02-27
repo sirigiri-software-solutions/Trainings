d57858b1a76e23e1ce151ec976acd2597861d750
NARESHKARRI <Naresh.karri@sirigiritrainings.onmicrosoft.com>
2024-02-27 19:24:52 +0530

    js tasjk completed

---
+++ DAY8/js1.js
@@ -0,0 +1,35 @@
+var a;
+console.log(16)
+
+var b="4";
+console.log(b)
+var c=16;
+console.log(c)
+var d=4;
+console.log(d);
+var e='volvo';
+console.log(e);
+
+console.log(typeof(b));
+console.log(typeof(a));
+console.log(typeof(c));
+console.log(typeof(e));
+console.log(typeof(d));
+
+var f="MAHENDRA";
+var g="XUV";
+var h=700;
+console.log(f+"-"+g+" "+h);
+ var side=6;
+function cube(){
+    console.log(side*side*side)
+}
+cube()
+
+var i=77;
+var cel;
+function celcius(){
+    cel=(5/9)*(i-32)
+    console.log(cel)
+}
+celcius();
